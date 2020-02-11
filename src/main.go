f640e7c55a3ee4db38903c2df4e5d06162e19a58
Quinten Palmer <quintenpalmer@gmail.com>
2020-02-11 17:34:55 -0500

    List full names of all routes
    This completes question one of the interview take-home assignment.
    
    Basic outline of the structure:
    
    * RouteWrapper is the entire payload for the `/routes` endpoint.
    * Route is what the RouteWrapper is comprised of; note that we only
        include the fields that we will concern ourselves with for question
        one This is to reduce noise on the structure, and we will be
        augmenting it as we build more functionality.
    * Attribute is the sub-object of Route that has the `long_name` we want
        to display for question one.
    * RouteRailType is an enumerated type for the different rail types that
        the `/route` endpoint can filter by. We only include the "Light
        Rail" and "Heavy Rail" types that we will be filtering down to.
    * list_light_and_heavy_rail_routes contains the logic to request the
        light and heavy rail routes, and then iterate over them to display
        their full names.
    
    We filter for the heavy and light rails, as documented in the comment at
    the beginning of the `list_light_and_heavy_rail_routes` function.

--- src/main.go
+++ src/main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("initial commit")
+	if err := list_light_and_heavy_rail_routes(); err != nil {
+		panic(err)
+	}
+}
+
+type RouteWrapper struct {
+	Data []Route `json:"data"`
+}
+
+type Route struct {
+	Attribute Attribute `json:"attributes"`
+}
+
+type Attribute struct {
+	LongName string `json:"long_name"`
+}
+
+type RouteRailType int
+
+// Per https://api-v3.mbta.com/docs/swagger/index.html#/Route/ApiWeb_RouteController_index
+// The "Light Rail" is 0 and the "Heavy Rail" is 1.
+// There are more rail types, but we are not concerning ourselves with it for this problem.
+const (
+	RouteRailTypeLightRail RouteRailType = iota
+	RouteRailTypeHeavyRail
+)
+
+func list_light_and_heavy_rail_routes() error {
+	// Question 1 mentions how we can filter for the rail types on the query, or could filter after having
+	// consumed the response. To save on retrieving data that we don't need, we are asking the server to filter
+	// for us. Given how the filter types are documented with their own type, I feel this speaks reasonably well
+	// as to what is happening with the query params going into the request.
+	url := fmt.Sprintf("https://api-v3.mbta.com/routes?filter[type]=%d,%d", RouteRailTypeLightRail, RouteRailTypeHeavyRail)
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	wrapper := RouteWrapper{}
+	decoder := json.NewDecoder(resp.Body)
+	if err := decoder.Decode(&wrapper); err != nil {
+		return err
+	}
+
+	fmt.Println("The Heavy Rail and Light Rail Routes are:")
+	for _, route := range wrapper.Data {
+		fmt.Println(route.Attribute.LongName)
+	}
+
+	return nil
 }

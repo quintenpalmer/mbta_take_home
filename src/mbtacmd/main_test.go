82745a14ea85f54f761086a6731d4fb91c306199
Quinten Palmer <quintenpalmer@gmail.com>
2020-02-11 20:57:05 -0500

    Add tests for `get_heavy_and_light_routes`
    This tests the receiving of the route types as well as the output in the
    happy and sad paths.

--- src/mbtacmd/main_test.go
+++ src/mbtacmd/main_test.go
@@ -71,3 +71,53 @@ func Test_list_light_and_heavy_rail_routes(t *testing.T) {
 		}
 	})
 }
+
+func Test_get_heavy_and_light_routes(t *testing.T) {
+	t.Run("happy path", func(t *testing.T) {
+		wrapper := RouteWrapper{
+			Data: []Route{
+				{
+					Attribute: RouteAttribute{
+						LongName: "mock route name 1",
+					},
+				},
+				{
+					Attribute: RouteAttribute{
+						LongName: "mock route name 2",
+					},
+				},
+			},
+		}
+
+		mockAPI := &MockMBTAWebServer{
+			ReturnRouteWrapper: wrapper,
+		}
+
+		routes, err := get_heavy_and_light_routes(mockAPI)
+		if err != nil {
+			t.Error("did not expect an error")
+		}
+		if !reflect.DeepEqual(wrapper, routes) {
+			t.Errorf("expected %s to be equal to %s", wrapper, routes)
+		}
+		if *mockAPI.RecvType1 != RouteRailTypeLightRail {
+			t.Errorf("expected received %d to be %d", mockAPI.RecvType1, RouteRailTypeLightRail)
+		}
+		if *mockAPI.RecvType2 != RouteRailTypeHeavyRail {
+			t.Errorf("expected received %d to be %d", mockAPI.RecvType2, RouteRailTypeHeavyRail)
+		}
+	})
+
+	t.Run("sad path", func(t *testing.T) {
+		myErr := errors.New("custom mock error")
+
+		mockAPI := &MockMBTAWebServer{
+			ReturnRouteWrapperError: myErr,
+		}
+
+		_, err := get_heavy_and_light_routes(mockAPI)
+		if err != myErr {
+			t.Errorf("expected error %s to be %s", myErr, err)
+		}
+	})
+}

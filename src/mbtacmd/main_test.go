ab1dca34f53408e8b9c25b0f69ec76dd3867c88f
Quinten Palmer <quintenpalmer@gmail.com>
2020-02-11 21:11:51 -0500

    Include tests for `collect_stop_data`
    This includes heavy happy-path testing for the min/max/stop-route-map
    computations, as well as sad-path testing for the route and stop
    lookups.
    
    Again, we are skipping tests for `print_stop_data` as it is just the
    interface with standard out and is being treated as lower priority. If
    we feel we need this tested, we can revisit.
    
    We will include tests for its helper `build_route_list_name` though;
    that will be in the next commit.

--- src/mbtacmd/main_test.go
+++ src/mbtacmd/main_test.go
@@ -13,7 +13,7 @@ type MockMBTAWebServer struct {
 	ReturnRouteWrapperError error
 
 	RecvRoutes             []Route
-	ReturnStopWrapper      StopWrapper
+	ReturnStopWrapper      map[string]StopWrapper
 	ReturnStopWrapperError error
 }
 
@@ -25,7 +25,7 @@ func (c *MockMBTAWebServer) GetRoutes(type1 RouteRailType, type2 RouteRailType) 
 
 func (c *MockMBTAWebServer) GetStops(route Route) (StopWrapper, error) {
 	c.RecvRoutes = append(c.RecvRoutes, route)
-	return c.ReturnStopWrapper, c.ReturnStopWrapperError
+	return c.ReturnStopWrapper[route.ID], c.ReturnStopWrapperError
 }
 
 func Test_list_light_and_heavy_rail_routes(t *testing.T) {
@@ -116,6 +116,150 @@ func Test_get_heavy_and_light_routes(t *testing.T) {
 		}
 
 		_, err := get_heavy_and_light_routes(mockAPI)
+		if err != myErr {
+			t.Errorf("expected error %s to be %s", myErr, err)
+		}
+	})
+}
+
+func Test_collect_stop_data(t *testing.T) {
+	t.Run("happy path", func(t *testing.T) {
+		mockAPI := &MockMBTAWebServer{
+			ReturnRouteWrapper: RouteWrapper{
+				Data: []Route{
+					{
+						ID: "route key 1",
+						Attribute: RouteAttribute{
+							LongName: "mock route name 1",
+						},
+					},
+					{
+						ID: "route key 2",
+						Attribute: RouteAttribute{
+							LongName: "mock route name 2",
+						},
+					},
+				},
+			},
+			ReturnStopWrapper: map[string]StopWrapper{
+				"route key 1": StopWrapper{
+					Data: []Stop{
+						{
+							ID: "stop key 1",
+							Attribute: StopAttribute{
+								Name: "mock stop name 1",
+							},
+						},
+					},
+				},
+				"route key 2": StopWrapper{
+					Data: []Stop{
+						{
+							ID: "stop key 1",
+							Attribute: StopAttribute{
+								Name: "mock stop name 1",
+							},
+						},
+						{
+							ID: "stop key 2",
+							Attribute: StopAttribute{
+								Name: "mock stop name 2",
+							},
+						},
+					},
+				},
+			},
+		}
+
+		expectedMinMaxData := MinMaxData{
+			Min:      1,
+			MinRoute: "mock route name 1",
+			Max:      2,
+			MaxRoute: "mock route name 2",
+		}
+
+		expectedStopRoutes := map[Stop][]Route{
+			{
+				ID: "stop key 1",
+				Attribute: StopAttribute{
+					Name: "mock stop name 1",
+				},
+			}: []Route{
+				{
+					ID: "route key 1",
+					Attribute: RouteAttribute{
+						LongName: "mock route name 1",
+					},
+				},
+				{
+					ID: "route key 2",
+					Attribute: RouteAttribute{
+						LongName: "mock route name 2",
+					},
+				},
+			},
+			{
+				ID: "stop key 2",
+				Attribute: StopAttribute{
+					Name: "mock stop name 2",
+				},
+			}: []Route{
+				{
+					ID: "route key 2",
+					Attribute: RouteAttribute{
+						LongName: "mock route name 2",
+					},
+				},
+			},
+		}
+
+		minMaxData, stopRoutes, err := collect_stop_data(mockAPI)
+		if err != nil {
+			t.Error("did not expect an error")
+		}
+		if expectedMinMaxData != minMaxData {
+			t.Errorf("expected %+v to be equal to %+v", expectedMinMaxData, minMaxData)
+		}
+		if !reflect.DeepEqual(expectedStopRoutes, stopRoutes) {
+			t.Errorf("expected %+v to be equal to %+v", expectedStopRoutes, stopRoutes)
+		}
+	})
+
+	t.Run("sad path - route lookup fails", func(t *testing.T) {
+		myErr := errors.New("custom mock error")
+
+		mockAPI := &MockMBTAWebServer{
+			ReturnRouteWrapperError: myErr,
+		}
+
+		_, _, err := collect_stop_data(mockAPI)
+		if err != myErr {
+			t.Errorf("expected error %s to be %s", myErr, err)
+		}
+	})
+
+	t.Run("sad path - stop lookup fails", func(t *testing.T) {
+		myErr := errors.New("custom mock error")
+
+		mockAPI := &MockMBTAWebServer{
+			ReturnRouteWrapper: RouteWrapper{
+				Data: []Route{
+					{
+						Attribute: RouteAttribute{
+							LongName: "mock route name 1",
+						},
+					},
+					{
+						Attribute: RouteAttribute{
+							LongName: "mock route name 2",
+						},
+					},
+				},
+			},
+			ReturnStopWrapperError: myErr,
+		}
+
+		_, _, err := collect_stop_data(mockAPI)
 		if err != myErr {
 			t.Errorf("expected error %s to be %s", myErr, err)
 		}
